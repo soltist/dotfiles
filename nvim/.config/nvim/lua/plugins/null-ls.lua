2f911d74fd7fb2ac096cc797ec2192a2e166fae8
Tomas Soltis <tomas.soltis1@gmail.com>
2023-06-29 15:32:02 -0400

    update timeout for null-ls

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -9,16 +9,15 @@ return {
         -- The linter that needs to be added is loaded here
         -- nls_diagnostics.golangci_lint.with(require("plugins.extras.lsp.null-ls.diagnostics.golangci_lint")),
         -- nls_diagnostics.buf.with(require("plugins.extras.lsp.null-ls.diagnostics.buf")),
-        nls_diagnostics.ruff.with({ only_local = ".venv/bin" }),
+        nls_diagnostics.ruff.with({ only_local = ".venv/bin", timeout = 5000 }),
         -- nls_diagnostics.mypy.with(require("plugins.extras.mypy")),
-        nls_diagnostics.mypy.with({ only_local = ".venv/bin" }), --.with(require("plugins.extras.mypy")),
+        nls_diagnostics.mypy.with({ only_local = ".venv/bin", timeout = 5000 }), --.with(require("plugins.extras.mypy")),
       }
       local formatting = {
         -- The formatter that needs to be added is loaded here
         -- nls_formatting.goimports.with(require("plugins.extras.lsp.null-ls.formatting.goimports")),
         -- nls_formatting.buf.with(require("plugins.extras.lsp.null-ls.formatting.buf")),
         nls_formatting.black.with({ only_local = ".venv/bin", timeout = 5000 }),
-        nls_formatting.isort.with({ only_local = ".venv/bin", timeout = 5000 }),
       }
       if type(opts.sources) == "table" then
         opts.sources = vim.list_extend(opts.sources, diagnostics)
