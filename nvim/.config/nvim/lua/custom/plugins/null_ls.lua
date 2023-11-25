f106f69270309f26d82923180a1c8d0d3373ae62
Tomas Soltis <tomas.soltis1@gmail.com>
2023-11-25 22:42:02 +0100

    disable null-ls

--- nvim/.config/nvim/lua/custom/plugins/null_ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/null_ls.lua
@@ -1,18 +1,18 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "mason.nvim" },
+  'jose-elias-alvarez/null-ls.nvim',
+  enabled = false,
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = { 'mason.nvim' },
   opts = function()
-    local nls = require("null-ls")
+    local nls = require 'null-ls'
     return {
-      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git'),
       sources = {
         nls.builtins.formatting.fish_indent,
         nls.builtins.diagnostics.fish,
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.shfmt,
-        nls.builtins.diagnostics.mypy.with({ only_local = ".venv/bin", timeout = 5000 }),
-        nls.builtins.formatting.black.with({ only_local = ".venv/bin", timeout = 5000 }),
+        nls.builtins.diagnostics.mypy.with { only_local = '.venv/bin', timeout = 5000 },
       },
     }
   end,
